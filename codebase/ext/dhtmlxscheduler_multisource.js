86e75d0a498492cce80bf86fb4f825291202996e
Phil Hannent <phil@elmstonesystems.co.uk>
2014-05-20 15:05:46 +0100

    Remove unneeded files which belong to someone else.

--- codebase/ext/dhtmlxscheduler_multisource.js
+++
@@ -1,5 +0,0 @@
-/*
-Copyright Dinamenta UAB. http://www.dhtmlx.com
-To use this component please contact sales@dhtmlx.com to obtain license
-*/
-(function(){function e(a){var b=function(){};b.prototype=a;return b}var d=scheduler._load;scheduler._load=function(a,b){a=a||this._load_url;if(typeof a=="object")for(var f=e(this._loaded),c=0;c<a.length;c++)this._loaded=new f,d.call(this,a[c],b);else d.apply(this,arguments)}})();
